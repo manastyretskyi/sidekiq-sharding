d8508bdc7e81c05b3fcf80e95edeb1421e1d844b
Liubomyr Manastyretskyi <manastyretskyi@gmail.com>
2022-04-16 23:04:35 +0300

    Initial commit

---
+++ lib/sidekiq/sharding.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sharding/version"
+
+module Sidekiq
+  module Sharding
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
